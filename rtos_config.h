005ab63c40a31cbe7ae224c304d9614ae06bc564
Albertoie710231 <ie710231@iteso.mx>
2020-09-16 17:03:39 -0500

    Primer comentario

---
+++ rtos_config.h
@@ -0,0 +1,31 @@
+/*
+ * rtos_config.h
+ *
+ *  Created on: 16 sep. 2020
+ *      Author: alberto
+ */
+
+#ifndef RTOS_CONFIG_H_
+#define RTOS_CONFIG_H_
+
+/*! @brief Tick period */
+#define RTOS_TIC_PERIOD_IN_US 		(1000)
+
+/*! @brief Stack size for each task */
+#define RTOS_STACK_SIZE				(100)
+
+/*! @brief Max number of tasks for runtime */
+#define RTOS_MAX_NUMBER_OF_TASKS	(10)
+
+/*! @brief Is alive configuration */
+#define RTOS_ENABLE_IS_ALIVE
+#ifdef RTOS_ENABLE_IS_ALIVE
+/*! @brief Is alive signal port */
+#define RTOS_IS_ALIVE_PORT			E
+/*! @brief Is alive signal pin */
+#define RTOS_IS_ALIVE_PIN			6
+/*! @brief Is alive signal period */
+#define RTOS_IS_ALIVE_PERIOD_IN_US  (1000000)
+#endif
+
+#endif /* RTOS_CONFIG_H_ */
